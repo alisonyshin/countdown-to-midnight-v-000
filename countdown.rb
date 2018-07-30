ec7207c4e8cab35c31ffc6f585b8ac133423506f
Alison Shin <alisonyshin@gmail.com>
2018-07-30 01:44:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,8 @@ end
 def countdown(seconds_to_midnight)
   until seconds_to_midnight == 0
     puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
     sleep(1.0)
+    seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
 end
