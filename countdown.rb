3d77e8aa52f155c24019e7467dd57de81b28aa78
Alison Shin <alisonyshin@gmail.com>
2018-07-30 01:41:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,6 @@ def countdown(seconds_to_midnight)
 end
 #countdown(10)
 
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
 
 # def countdown_with_sleep(number)
 #     until number==0
