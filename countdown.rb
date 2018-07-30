ab9bc1607ac8ab149de3336eb636e707bdae6ce1
Alison Shin <alisonyshin@gmail.com>
2018-07-30 01:37:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,12 +8,12 @@ def countdown(number)
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-    until number==0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-    sleep(1.0)
-  end
-  puts "HAPPY NEW YEAR!"
-end
+# def countdown_with_sleep(number)
+#     until number==0
+#     puts "#{number} SECOND(S)!"
+#     number -= 1
+#     sleep(1.0)
+#   end
+#   puts "HAPPY NEW YEAR!"
+# end
 
